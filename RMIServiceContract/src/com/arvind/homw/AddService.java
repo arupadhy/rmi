e72a63ff97f253e7a8f30f19047144a3ddb3cc78
arvind upadhyay <arvind.charlotte@gmail.com>
2015-05-10 19:39:50 -0400

    sample rmi service contract project

---
+++ RMIServiceContract/src/com/arvind/homw/AddService.java
@@ -0,0 +1,9 @@
+package com.arvind.homw;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface AddService extends Remote {
+ 
+	int add(int a, int b) throws RemoteException;
+}
